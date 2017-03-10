6e6c2b8c4618f53304f53884de376b4a09b87c38
vvrsk <karthikvvrs@gmail.com>
2017-03-10 09:31:09 -0600

    Commit to Analysis of Towed Vehicles
    Commit #3

--- Analysis of Towed Vehicles in Chicago/server.R
+++ Analysis of Towed Vehicles in Chicago/server.R
@@ -1,6 +1,12 @@
 library(shiny)
 library(plotly)
 
+library(readr)
+towed_vehicles <- read_csv("./data/Towed_Vehicles.csv", 
+                           col_types = cols(Model = col_skip(), 
+                                            Plate = col_skip(), State = col_skip(), 
+                                            Style = col_skip(), `Tow Facility Phone` = col_skip()))
+
 shinyServer(
         function(input, output) {  
                 
@@ -31,7 +37,17 @@ shinyServer(
         output$table <- renderDataTable(towed_vehicles)
         
         
-        txt <- "<br/> <font size='4'>Hello, Thanks for Coming here!! <br/> </font size>
+        txt <- " <b> <font size='4'> Introduction </b></ font size>
+                <br/>
+
+        <p>The following visual analysis is a visual depiction of the Towed Vehicles data in City of Chicago. This can be used as a pre analysis of finding the make of most towed vehicles and their color and where they have been towed to (Auto Pounds). </p>
+
+        <b><font size='4'>Data</b></ font size>
+
+        <p>The data for this can be found at the City of Chicago's Website(https://data.cityofchicago.org/Transportation/Towed-Vehicles/ygr5-vcbg)</p>
+        For this analysis I considered the data for the last 4 months (Dec 2016 - Feb 2017) and can be seen in the Data Table Tab of the Graph.</p>
+
+                <br/> <font size='4'>Hello, Thanks for Coming here!! <br/> </font size>
                 <ul>
                 <li>This is an analyis of how Towed Vechicles are stacked in the 4 different Auto Pounds of <b>City of Chicago</b>. </li>
                 <li>For this analysis I considered the data for the last 4 months (Dec 2016 - Feb 2017) and can be seen in the <b>Data Table</b> Tab of the Graph.</li> 
@@ -39,8 +55,19 @@ shinyServer(
                 <li>The <b>Auto Pounds Aggregate</b> gives the Aggrate analysis of Auto ponds and the <b>Location Analysis</b> gives the distribution of different vechicles available in each Auto Pound.</li>
                 </ul>"
         
+        txt2<- " 
+
+
+<br/> <b><font size='4'>Observations<br/> </b></font size>
+
+From the graphs, we can observe many things, but these are the key observations made my me.
+<ul>
+<li> Most of the Vehicles ar econcentrated in just two of the 4 available Auto Pounds </li>
+<li> The reason for the concentration of the cars being more in the other two ponds could be many.</li>
+<li> The reason why the two pounds have lower concentration is because they are both fundamentally smaller than the other two and one of them is in Down Town of Chicago and other is near O'Hare Airport(1 hr drive from Down Town) both of which are not great locations for holding th etowed vehicles for security and logistic purposes.</li>
+<li> This analysis can be used to find if there is any relation between the Car and its color on the locaction where it is held and to find out the most number of vehicles that are held ina  Auto Pound </li>"
         output$summary <- renderUI({ 
-               HTML(paste(txt))
+               HTML(paste(txt,txt2,sep = '<br/>'))
         })
         
         
--- Analysis of Towed Vehicles in Chicago/ui.R
+++ Analysis of Towed Vehicles in Chicago/ui.R
@@ -2,12 +2,18 @@ library(shiny)
 library(plotly)
 
 
+library(readr)
+towed_vehicles <- read_csv("./data/Towed_Vehicles.csv", 
+                           col_types = cols(Model = col_skip(), 
+                                            Plate = col_skip(), State = col_skip(), 
+                                            Style = col_skip(), `Tow Facility Phone` = col_skip()))
+
 make <- unique(sort(towed_vehicles$Make))
 color<- unique(sort(towed_vehicles$Color))
 tow_to <-unique(sort(towed_vehicles$`Towed to Address`))
 
 shinyUI(fluidPage( 
-        headerPanel("Towed Vehicles Visualizer"),  
+        headerPanel("Visual Analysis of Towed Vehicles in City of Chicago"),  
         #sidebarPanel(
         #        h3("Control Subpanel"), 
         #        selectInput("type", "Select Type of vehicle", choices = make, selected=name[1]),
@@ -18,8 +24,8 @@ shinyUI(fluidPage(
                 tabsetPanel(
                 tabPanel("Summmary",htmlOutput("summary")),
                 tabPanel("Make analysis",h3(textOutput("add")),
-                            selectInput("type", "Select Type of vehicle", choices = make, selected=name[1]),
-                            selectInput("color", "Select color code", choices = tow_to, selected=name[2]),
+                            selectInput("type", "Select Type of vehicle", choices = make),
+                            selectInput("color", "Select color code", choices = tow_to),
                             plotlyOutput('myChart',height = "470px")
                          ),
                 tabPanel("Location Analysis",plotlyOutput('location_chart',height = "550px")),
